4166308790473827a9699b919de7abdcabad5d22
Baidyanath Kundu <kundubaidya99@gmail.com>
2023-03-01 09:38:01 +0100

    Send back scope instead of the name of the templ

--- python/cppyy/_cpython_cppyy.py
+++ python/cppyy/_cpython_cppyy.py
@@ -74,7 +74,7 @@ class Template(object):  # expected/used by ProxyWrappers.cxx in CPyCppyy
             args = args[0]
 
       # construct the type name from the types or their string representation
-        newargs = [self.__name__] if self.__name__ != "vector" else ["std::vector"]
+        newargs = [self.__scope__]
         for arg in args:
             if type(arg) == str:
                 arg = ','.join(map(lambda x: x.strip(), arg.split(',')))
