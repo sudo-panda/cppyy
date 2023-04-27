fc64db73639b1f8f54f808e36ed92e375220ac5b
Vassil Vassilev <v.g.vassilev@gmail.com>
2023-04-27 13:10:33 +0000

    Test crashes on the bots

--- test/test_streams.py
+++ test/test_streams.py
@@ -32,7 +32,7 @@ class TestSTDStreams:
 
         assert not (cppyy.gbl.std.cout is None)
 
-    @mark.xfail
+    @mark.crashes
     def test03_consistent_naming_if_char_traits(self):
         """Naming consistency if char_traits"""
 
